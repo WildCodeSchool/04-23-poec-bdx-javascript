1b6a5f563cba294c6b791af27bb59a4520dc0eb0
pilou <p-l.b@hotmail.com>
2024-04-05 12:11:33 +0200

    ended course2

---
+++ course2.js
@@ -0,0 +1,66 @@
+
+/**
+ * FONCTIONS
+ */
+
+// déclaration fonction : méthode 1
+function sayHello(name) {
+    return "salut bg " + name;
+};
+// déclaration fonction : méthode 2
+const sayHello2 = (name) => {
+    return "salut bg " + name;
+};
+
+
+
+const result = sayHello("argument omg"); // salut bg argument omg
+console.log(result);
+
+
+
+/**
+ * TABLEAUX
+ */
+
+const pokemonList = [
+    { name: "carapuce", type: "water" },
+    { name: "salamèche", type: "fire" }
+];
+
+
+const pokemonListFiltered = pokemonList.filter((pokemon) => pokemon.type === "fire");
+console.log(pokemonListFiltered);
+
+/**
+ * BOUCLES
+ */
+
+for(let i = 0; i < pokemonList.length; i++) {
+    const current = pokemonList[i];
+    console.log(current.name + " " + current.type);
+
+}
+
+/**
+ * OBJETS
+ */
+
+const person = {
+    name: "Norbert",
+    age: 42,
+    getAge: function() {
+        return this.age;
+    }
+};
+
+
+console.log(person.age);
+console.log(person.getAge());
+
+person.address = "adresse";
+delete person.address;
+person.age = 56;
+
+const entries = Object.entries(person);
+console.log(entries);
